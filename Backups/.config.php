e37aa5f4afeae8ea6aa9f3e19b28be7e149ccf98
setknns <sthknns@gmail.com>
2025-12-15 13:02:16 +0100

    /gesprek endpoint /backup/.config.php

--- Backups/.config.php
+++ Backups/.config.php
@@ -1,6 +1,6 @@
 <?php
 /* Tijdelijke admin configuratie – niet verwijderen! */
-$admin_user = “flag2{admin_username}”;
+$admin_user = “flag2{HouthavensAdmin1}”;
 $theme = “dark”;
 $debug = false;
 ?>
